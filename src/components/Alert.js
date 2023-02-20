9cad79a5517f2a1521bfc003eb264037cbdaecca
Guillaume Mourier <guillaume@meilisearch.com>
2023-02-20 18:19:47 +0100

    remove dark: directives

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import { ExclamationTriangleIcon } from '@heroicons/react/20/solid'
 
 function Alert() {
     return (
-        <div className="rounded-md text-sm dark:bg-gray-800 p-4 mt-8 mb-8">
+        <div className="rounded-md text-sm bg-gray-800 p-4 mt-8 mb-8">
             <div className="flex">
                 <div className="flex-shrink-0">
                     <ExclamationTriangleIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
